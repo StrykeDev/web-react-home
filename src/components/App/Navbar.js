cbfd5ca920ded97b4ac3ebd1824d3f483a0a2911
attiasbarak <attias.barak@gmail.com>
2020-01-09 15:17:59 +0200

    Upgraded navbar profile dropdown

--- src/components/App/Navbar.js
+++ src/components/App/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 import { PagesContext } from "../../contexts/PagesContext";
@@ -10,30 +10,50 @@ const NavBar = props => {
   const { auth } = useContext(AuthContext);
 
   const user = auth.current ? (
-    <NavDropdown title={auth.current}>
-      <NavDropdown.Item eventKey="profile" as={Link} to="/profile">
-        Profile
-      </NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item eventKey="sign-out" as={Link} to="/sign-out">
-        Sign Out
-      </NavDropdown.Item>
-    </NavDropdown>
+    <>
+      <NavDropdown
+        title={<span className="text-white mx-2">{auth.current}</span>}
+        className="d-none d-md-block"
+      >
+        <NavDropdown.Item eventKey="profile" as={NavLink} to="/profile">
+          Profile
+        </NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item eventKey="sign-out" as={NavLink} to="/sign-out">
+          Sign Out
+        </NavDropdown.Item>
+      </NavDropdown>
+
+      <span className="d-md-none">
+        <Navbar.Text className="text-white">{auth.current}</Navbar.Text>
+        <Nav.Link eventKey="profile" as={NavLink} to="/profile">
+          Profile
+        </Nav.Link>
+        <Nav.Link eventKey="sign-out" as={NavLink} to="/sign-out">
+          Sign Out
+        </Nav.Link>
+      </span>
+    </>
   ) : (
-    <NavDropdown title="Hello Stranger">
-      <NavDropdown.Item eventKey="sign-in" as={Link} to="/sign-in">
+    <>
+      <Nav.Link eventKey="sign-in" as={NavLink} to="/sign-in">
         Sign in
-      </NavDropdown.Item>
-      <NavDropdown.Item eventKey="sign-up" as={Link} to="/sign-up">
+      </Nav.Link>
+      <Nav.Link eventKey="sign-up" as={NavLink} to="/sign-up">
         Sign up
-      </NavDropdown.Item>
-    </NavDropdown>
+      </Nav.Link>
+    </>
   );
 
   const links = pages.map(page => {
     if (page.nav) {
       return (
-        <Nav.Link key={page.page} eventKey={page.page} as={Link} to={page.url}>
+        <Nav.Link
+          key={page.page}
+          eventKey={page.page}
+          as={NavLink}
+          to={page.url}
+        >
           {page.page}
         </Nav.Link>
       );
@@ -50,13 +70,14 @@ const NavBar = props => {
       collapseOnSelect={true}
     >
       <Container>
-        <Nav.Link eventKey="logo" as={Link} to="/">
+        <Nav.Link eventKey="logo" as={NavLink} to="/">
           <Navbar.Brand>¯\_(ツ)_/¯</Navbar.Brand>
         </Nav.Link>
 
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="mr-auto">{links}</Nav>
+          <NavDropdown.Divider />
           <Nav className="ml-auto">{user}</Nav>
         </Navbar.Collapse>
       </Container>
