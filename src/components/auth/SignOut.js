af503ad2b2753ecb53e6649813d86b81fab9c3bb
attiasbarak <attias.barak@gmail.com>
2020-01-09 13:12:23 +0200

    New sign out page

--- src/components/auth/SignOut.js
+++ src/components/auth/SignOut.js
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { Container, Button } from "react-bootstrap";
 
 const SignOut = props => {
   const { dispatch } = useContext(AuthContext);
-  dispatch({ type: "LOGOUT" });
-  window.location.href = "/";
-  return <Redirect to="/" />;
+
+  const signOut = () => {
+    dispatch({ type: "LOGOUT" });
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="py-5 text-center">
+      <h1 className="display-3">WARNING</h1>
+      <p className="lead">
+        Signing out will delete all local data which will reset the application.
+      </p>
+      <Button variant="outline-danger" onClick={signOut}>
+        Delete Local Data And Sign Out
+      </Button>
+    </div>
+  );
 };
 
 export default SignOut;
