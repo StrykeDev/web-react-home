866b06ec294b3b990098207b575569b7d52cceac
attiasbarak <attias.barak@gmail.com>
2020-01-09 14:38:30 +0200

    Fixed navbar collapse

--- src/components/App/Navbar.js
+++ src/components/App/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
 
 import { PagesContext } from "../../contexts/PagesContext";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -11,20 +11,20 @@ const NavBar = props => {
 
   const user = auth.current ? (
     <NavDropdown title={auth.current}>
-      <NavDropdown.Item as={Link} to="/profile">
+      <NavDropdown.Item eventKey="profile" as={Link} to="/profile">
         Profile
       </NavDropdown.Item>
       <NavDropdown.Divider />
-      <NavDropdown.Item as={Link} to="/sign-out">
+      <NavDropdown.Item eventKey="sign-out" as={Link} to="/sign-out">
         Sign Out
       </NavDropdown.Item>
     </NavDropdown>
   ) : (
     <NavDropdown title="Hello Stranger">
-      <NavDropdown.Item as={Link} to="/sign-in">
+      <NavDropdown.Item eventKey="sign-in" as={Link} to="/sign-in">
         Sign in
       </NavDropdown.Item>
-      <NavDropdown.Item as={Link} to="/sign-up">
+      <NavDropdown.Item eventKey="sign-up" as={Link} to="/sign-up">
         Sign up
       </NavDropdown.Item>
     </NavDropdown>
@@ -33,22 +33,29 @@ const NavBar = props => {
   const links = pages.map(page => {
     if (page.nav) {
       return (
-        <Nav.Link as={NavLink} to={page.url} key={page.page}>
+        <Nav.Link key={page.page} eventKey={page.page} as={Link} to={page.url}>
           {page.page}
         </Nav.Link>
       );
     }
-    return "";
+    return null;
   });
 
   return (
-    <Navbar bg="dark" variant="dark" expand="md" fixed="top" >
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      fixed="top"
+      collapseOnSelect={true}
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          ¯\_(ツ)_/¯
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
+        <Nav.Link eventKey="logo" as={Link} to="/">
+          <Navbar.Brand>¯\_(ツ)_/¯</Navbar.Brand>
+        </Nav.Link>
+
+        <Navbar.Toggle />
+        <Navbar.Collapse>
           <Nav className="mr-auto">{links}</Nav>
           <Nav className="ml-auto">{user}</Nav>
         </Navbar.Collapse>
