a854285ac6206833d0a3282b62ac47524fd9020f
attiasbarak <attias.barak@gmail.com>
2020-01-10 15:25:50 +0200

    Optimized weather context

--- src/contexts/WeatherContext.js
+++ src/contexts/WeatherContext.js
@@ -6,39 +6,46 @@ export const WeatherContext = createContext();
 const WeatherContextProvider = props => {
   const [lastUpdate, setLastUpdate] = useState(Date());
   const [weather, setWeather] = useState();
-  const [location, setLocation] = useState({ lat: 0, lon: 0 });
   const appid = "eacfa3060655612bc2d28b3da22f78b6";
-  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${location.lat}&lon=${location.lon}&appid=${appid}`;
 
-  const getCurrentLocation = () => {
-    if (navigator.geolocation) {
-      return navigator.geolocation.getCurrentPosition(position => {
-        setLocation({
-          lat: position.coords.latitude,
-          lon: position.coords.longitude
-        });
-      });
-    } else {
-      console.warn("Geolocation is not supported by this browser.");
-    }
-  };
-
-  useEffect(() => {
-    Axios.get(url).then(res => {
+  const fetchWeather = location => {
+    Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${location.lat}&lon=${location.lon}&appid=${appid}`
+    ).then(res => {
       if (res.status === 200) {
         setWeather(res.data);
         console.log("Weather data updated.");
       } else {
         console.error("Failed to fetch weather data.", res);
       }
-      getCurrentLocation();
     });
-  }, [url, lastUpdate]);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const location = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+          };
+          fetchWeather(location);
+        },
+        () => {
+          const location = {
+            lat: 0,
+            lon: 0
+          };
+          fetchWeather(location);
+        }
+      );
+    }
+  }, [lastUpdate]);
 
   window.setTimeout(() => setLastUpdate(Date()), 10 * 60 * 1000);
 
   return (
-    <WeatherContext.Provider value={{ weather, location, setLocation }}>
+    <WeatherContext.Provider value={{ weather }}>
       {props.children}
     </WeatherContext.Provider>
   );
