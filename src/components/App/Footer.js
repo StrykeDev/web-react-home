91d251239e82b8de46d82831a1a876cde31671bb
attiasbarak <attias.barak@gmail.com>
2020-01-10 20:34:17 +0200

    Improved the footer

--- src/components/App/Footer.js
+++ src/components/App/Footer.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container, Jumbotron } from "react-bootstrap";
 
+import iconIsrael from "./assats/icon_israel.svg";
+
 const Footer = props => {
   return (
-    <Jumbotron as="footer" fluid className="m-0 p-0 bg-light">
-      <Container className="d-flex justify-content-between py-2">
-        <p className="text-muted mx-2">
+    <Jumbotron as="footer" fluid className="m-0 py-3 mt-5 bg-light">
+      <Container className="d-flex justify-content-between align-items-center px-4">
+        <p className="text-muted">
           <small>Copyright © 2020 | attias.barak@gmail.com</small>
         </p>
-        <p className="text-muted mx-2">
-          <small>Useless Websites Group</small>
+        <p className="text-muted">
+          <img src={iconIsrael} alt="" height="16" className="m-2" />
+          <small>Made In Israel</small>
         </p>
       </Container>
     </Jumbotron>
