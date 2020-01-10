0e57ded297dbd9d2971cfa3e35d48708786da1b8
attiasbarak <attias.barak@gmail.com>
2020-01-10 22:15:16 +0200

    Improved profile and added confirmation to rss reset

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -4,7 +4,7 @@ import { Container, Jumbotron, Form, Button } from "react-bootstrap";
 import { AuthContext } from "../../../contexts/AuthContext";
 import { RSSContext } from "../../../contexts/RSSContext";
 
-import HomeHeader from "../Home/HomeHeader";
+import Popup from "./../../../helpers/Popup";
 
 import defaultProfileImage from "./assats/profile-image.png";
 
@@ -31,13 +31,31 @@ const Profile = props => {
     }
   };
 
+  const handleRestRSS = () => {
+    const popup = new Popup();
+    popup.showModal(
+      "Reset RSS Feed?",
+      "Are you sure that you want to reset the RSS feed to default?",
+      [
+        {
+          text: "Rest",
+          variant: "danger",
+          onClick: () => dispatch({ type: "RESET_FEED" })
+        },
+        {
+          text: "Cancel",
+          variant: "secondary",
+          onClick: () => {}
+        }
+      ]
+    );
+  };
+
   return (
     <>
-      <HomeHeader />
-
-      <Container className="d-flex flex-wrap">
+      <Container className="mt-5 d-flex flex-wrap">
         <div className="col-12 p-2">
-          <Jumbotron className="d-flex align-items-center flex-md-row flex-column p-3 pb-4 w-100 h-100">
+          <Jumbotron className="d-flex align-items-center flex-md-row flex-column p-3 pb-0 w-100 h-100">
             <img
               src={defaultProfileImage}
               alt=""
@@ -71,7 +89,6 @@ const Profile = props => {
             <p>Current feed: {provider ? provider.title : "None"}</p>
             <Form noValidate validated={validated} onSubmit={handleSubmitRSS}>
               <Form.Group controlId="feedUrl" className="m-1 mb-2">
-                <Form.Label>RSS Feed: </Form.Label>
                 <Form.Control
                   name="rssFeed"
                   type="text"
@@ -95,10 +112,7 @@ const Profile = props => {
                 >
                   Save
                 </Button>
-                <Button
-                  variant="secondary"
-                  onClick={() => dispatch({ type: "RESET_FEED" })}
-                >
+                <Button variant="secondary" onClick={handleRestRSS}>
                   Reset
                 </Button>
               </Form.Group>
