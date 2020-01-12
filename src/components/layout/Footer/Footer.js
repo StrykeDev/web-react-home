8f7f494643a05d2fa80d195d3a62745919a969fe
attiasbarak <attias.barak@gmail.com>
2020-01-12 11:44:30 +0200

    Replaced static year in footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -4,11 +4,13 @@ import { Container, Jumbotron } from "react-bootstrap";
 import iconIsrael from "./assats/icon_israel.svg";
 
 const Footer = props => {
+  const year = new Date().getFullYear();
+
   return (
     <Jumbotron as="footer" fluid className="m-0 py-3 mt-5 bg-light">
       <Container className="d-flex justify-content-between align-items-center px-4">
         <p className="text-muted">
-          <small>Copyright © 2020 | attias.barak@gmail.com</small>
+          <small>Copyright © {year} | attias.barak@gmail.com</small>
         </p>
         <p className="text-muted">
           <img src={iconIsrael} alt="" height="16" className="m-2" />
