e891f9c579c39d67fcc02afd1cfa08364da8d9d9
attiasbarak <attias.barak@gmail.com>
2020-01-12 17:25:01 +0200

    Improved page not found

--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -1,20 +1,42 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import { useLocation, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const PageNotFound = props => {
-  let location = useLocation();
   let history = useHistory();
 
+  const message = [
+    "Even AI couldn't find that page :O",
+    "Page not found.",
+    "Sorry, we can't find the page you were looking for.",
+    "Something is broken.",
+    "This link isn't working, try one of this 2 instead :",
+    "The page cannot be found.",
+    "404 - Page not found",
+    "ERROR 404!"
+  ];
+
   return (
-    <Container>
-      <div className="my-5 mx-md-5 w-100">
-        <h1 className="display-2">Oopsie!</h1>
-        <h4>Couldn't found that page :\</h4>
-        <p className="text-mute">
-          <small>404 couldn't find '{location.pathname}'.</small>
-        </p>
-        <Button variant="outline-primary" onClick={() => history.goBack()}>Go Back</Button>
+    <Container className="text-center my-5">
+      <h1 className="display-3">Oh No!</h1>
+      <p className="lead">
+        {message[Math.floor(Math.random() * message.length)]}
+      </p>
+      <div className="d-flex flex-md-row flex-column justify-content-around w-50 mx-auto">
+        <Button
+          className="flex-fill m-2"
+          variant="outline-primary"
+          onClick={() => history.push("/")}
+        >
+          Go Home
+        </Button>
+        <Button
+          className="flex-fill m-2"
+          variant="outline-secondary"
+          onClick={() => history.goBack()}
+        >
+          Go Back
+        </Button>
       </div>
     </Container>
   );
